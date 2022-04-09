61ff1ac42dfb4c1e01a105d05db33ab9ee3ab5f3
James Bui <jamesvo.bui@core4ce.com>
2022-04-09 12:13:55 -0400

    Updates Backend to support current projectList

--- backend/todo/serializers.py
+++ backend/todo/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from .models import Todo
+from .models import ProjectList
 
 # This module is responsible for converting our models to JSON so the front end can use it
 
-class TodoSerializer(serializers.ModelSerializer):
+class ProjectListSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Todo
+        model = ProjectList
         fields = ('id', 'title', 'description', 'completed', 'project_url', 'thumbnail_url', 'tags')
